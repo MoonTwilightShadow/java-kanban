a37798ee7c387b1142c151ca4454585bb4340935
alex <lazutin.leh@yandex.ru>
2024-01-15 15:31:57 +0300

    feat: исправлены замечания

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -1,6 +1,5 @@
 package history;
 
-import model.Node;
 import model.Task;
 
 import java.util.ArrayList;
@@ -38,10 +37,11 @@ public class InMemoryHistoryManager implements HistoryManager {
 		final Node t = tail;
 		final Node newNode = new Node(task, t, null);
 		tail = newNode;
-		if (t == null)
+		if (t == null) {
 			head = newNode;
-		else
+		} else {
 			t.next = newNode;
+		}
 
 		return newNode;
 	}
@@ -71,5 +71,17 @@ public class InMemoryHistoryManager implements HistoryManager {
 		}
 
 		return tasks;
+	}
+
+	public class Node {
+		public Task task;
+		public Node prev;
+		public Node next;
+
+		public Node(Task task, Node prev, Node next) {
+			this.task = task;
+			this.prev = prev;
+			this.next = next;
+		}
 	}
 }
